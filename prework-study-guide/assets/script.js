2b583235a81ca4858f07137bd8298416069ffc99
florianemil <117308886+florianemil@users.noreply.github.com>
2023-01-01 20:28:07 +0000

    final adjustments to js.

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,16 +1,22 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-var topics = "HTML,CSS,Git,Javascript";
-console.log(topics);
+var topics =['HTML','CSS','Git','Javascript'];
 
-if (topics === 'HTML') {
+for(var x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+ }
+/*
+
+if (topic === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topics === 'CSS') {
+} else if (topic === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topics === 'Git') {
+} else if (topic === 'Git') {
   console.log("Let's study Git!");
-} else if (topics === 'JavaScript') {
+} else if (topic === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
 }
+
+*/
+
+ 
