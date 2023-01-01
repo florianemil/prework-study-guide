9da94281b9c8bb2cfa0292b785ce86ac05833901
florianemil <117308886+florianemil@users.noreply.github.com>
2023-01-01 17:10:40 +0000

    color changed to card border

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,14 +1,15 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
-var topic = "HTML";
+var topics = "HTML,CSS,Git,Javascript";
+console.log(topics);
 
-if (topic === 'HTML') {
+if (topics === 'HTML') {
   console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
+} else if (topics === 'CSS') {
   console.log("Let's study CSS!");
-} else if (topic === 'Git') {
+} else if (topics === 'Git') {
   console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
+} else if (topics === 'JavaScript') {
   console.log("Let's study JavaScript!");
 } else {
   console.log('Please try again!');
