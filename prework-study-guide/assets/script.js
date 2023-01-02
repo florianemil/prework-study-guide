47fef1a4c3ce9b19a8cb40bb4630e7b858cb11a3
florianemil <117308886+florianemil@users.noreply.github.com>
2023-01-02 14:40:57 +0000

    js changed and html

--- prework-study-guide/assets/script.js
+++ prework-study-guide/assets/script.js
@@ -1,9 +1,9 @@
 var topics = ['HTML', 'CSS', 'GIT', 'JavaScript'];
 var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-function selectTopic() {
+function listTopics() {
   for (var x = 0; x < topics.length; x++) {
-    console.log(topics.length; [x]);
+    console.log(topics[x]);
 }
   }
 
@@ -21,7 +21,7 @@ function selectTopic() {
   }
 }
 
-console.log('Here are the topics we learned through Prework: ;');
+console.log('Here are the topics we learned through Prework: ');
 listTopics();
-console.log('Wich topic should we study first?;');
+console.log('Wich topic should we study first?');
 selectTopic();
